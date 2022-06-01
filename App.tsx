69b20333a4a7eef99c3c10619d99162e670de1dd
Subhadra-21 <021shubhagen@gmail.com>
2022-06-01 19:05:25 +0530

    Counter1

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import * as React from 'react';
+import { useState } from 'react';
 import './style.css';
+import { CounterProps } from './types';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Counter initCount={2} />
+    </div>
+  );
+}
+
+function Counter({ initCount }: CounterProps) {
+  const [count, setCount] = useState<number>(initCount);
+
+  function handleBtnClick(e: React.MouseEvent<HTMLButtonElement>): void {
+    setCount((count) => {
+      return count + 1;
+    });
+  }
+
+  return (
+    <div>
+      <h1>Counter value : {count}</h1>
+      <button onClick={handleBtnClick}>Increment</button>
     </div>
   );
 }
---
+++ types.ts
@@ -0,0 +1,5 @@
+type CounterProps = {
+  initCount: number;
+};
+
+export { CounterProps };
